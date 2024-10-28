3ded6c2e05b86993d4884881bc52ee452424d47d
setlaoui <setlaoui@gmail.com>
2024-10-28 13:08:24 +0100

    created sign up component, the form component, button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+<<<<<<< HEAD
 import CategoryMenu from "./components/category-menu/category-menu.component";
 
 const App = () => {
   return <CategoryMenu />;
+=======
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navegation from "./routes/home/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navegation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+>>>>>>> 661aff9 (created sign up component, the form component, button component)
 };
 
 export default App;
