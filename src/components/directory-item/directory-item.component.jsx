d4f9fab7897765a3688153068b5c5a55e70d512c
setlaoui <setlaoui@gmail.com>
2024-11-12 18:02:14 +0100

    cabrones

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,29 @@
-import './directory-item.styles.scss'
+import { useNavigate } from "react-router-dom";
+import "./directory-item.styles.scss";
 
-const DirectoryItem = ({category}) =>{
-    const {id, title, imageUrl} = category;
-    return (
-        <div className="directory-item-container" key={id}>
-          <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${imageUrl})`
-            }}
-          />
-          <div className="body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-    )
-}
+const DirectoryItem = ({ category }) => {
+  const { id, title, imageUrl, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHndler = () => navigate(route);
+  return (
+    <div
+      className="directory-item-container"
+      key={id}
+      onClick={onNavigateHndler}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
 
 export default DirectoryItem;
